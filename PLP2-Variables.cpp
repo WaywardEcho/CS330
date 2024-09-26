667f1bcfbf63620288db9fc5a80119bed8b459bc
echo <146795659+WaywardEcho@users.noreply.github.com>
2024-09-26 15:00:34 -0400

    Variable Code EX
    I apparently forgot to upload this

---
+++ PLP2-Variables.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main(){
+    int num;
+    num = 13;
+
+    float swim = 14.35;
+
+    double dou;
+    dou = 3.14;
+
+    char grade = 'A';
+
+    string hello = "Hello WAorld!";
+    bool pass = true;
+
+    cout << num;
+    cout << swim;
+    cout << dou;
+    cout << grade;
+    cout << hello;
+    cout << pass;
+    return 0;
+}
